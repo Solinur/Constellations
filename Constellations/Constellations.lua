82e74d6e506e9f0787dc1f39fa653ec339524899
Solinur <github@my-eg0.de>
2018-05-21 16:14:55 +0200

    update LAM release

--- Constellations/Constellations.lua
+++ Constellations/Constellations.lua
@@ -8,7 +8,7 @@ local pendata = {}
 Constellations = Constellations or {}
 local CST = Constellations
 CST.name 		= "Constellations"
-CST.version 	= "0.4.2"
+CST.version 	= "0.4.3"
 CST.debug = true
 
 local function Print(message, ...)
