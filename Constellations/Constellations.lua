a901d7b87f2fd307660451ff6df9d24e78c69967
Solinur <github@my-eg0.de>
2018-08-13 01:52:48 +0200

    fix version numbers

--- Constellations/Constellations.lua
+++ Constellations/Constellations.lua
@@ -8,7 +8,7 @@ local pendata = {}
 Constellations = Constellations or {}
 local CST = Constellations
 CST.name 		= "Constellations"
-CST.version 	= "0.4.3"
+CST.version 	= "0.4.4"
 CST.debug = true
 
 local function Print(message, ...)
