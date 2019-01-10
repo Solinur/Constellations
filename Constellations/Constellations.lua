518149e84e98e694384cdf2ec97989cffa2e909c
Solinur <github@my-eg0.de>
2019-01-10 22:03:41 +0100

    added some missing abilities reported by users

--- Constellations/Constellations.lua
+++ Constellations/Constellations.lua
@@ -8,7 +8,7 @@ local pendata = {}
 Constellations = Constellations or {}
 local CST = Constellations
 CST.name 		= "Constellations"
-CST.version 	= "0.4.7"
+CST.version 	= "0.4.8"
 CST.debug = true
 
 local function Print(message, ...)
