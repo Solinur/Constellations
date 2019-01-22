10e6dd9c806699d9a9d23485d4ef6f9870e80143
Solinur <github@my-eg0.de>
2019-01-22 01:32:26 +0100

    release reorganized libs

--- Constellations/Constellations.lua
+++ Constellations/Constellations.lua
@@ -8,7 +8,7 @@ local pendata = {}
 Constellations = Constellations or {}
 local CST = Constellations
 CST.name 		= "Constellations"
-CST.version 	= "0.4.8"
+CST.version 	= "0.4.9"
 CST.debug = true
 
 local function Print(message, ...)
