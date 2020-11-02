533d8ecabe7d5fe814e2ae63c346ac160bc70883
Solinur <github@my-eg0.de>
2020-11-03 00:08:34 +0100

    release, API bump

--- Constellations/Constellations.lua
+++ Constellations/Constellations.lua
@@ -8,7 +8,7 @@ local pendata = {}
 Constellations = Constellations or {}
 local CST = Constellations
 CST.name 		= "Constellations"
-CST.version 	= "1.1.4"
+CST.version 	= "1.1.5"
 CST.debug = false -- or GetDisplayName() == "@Solinur"
 
 local function Print(message, ...)
